10f26ffe51c0c8305f7703489ba383a9e707174d
robelpop <robelpopandro@gmail.com>
2023-05-07 16:26:41 -0200

    Fixed: loop not correct

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,14 +6,16 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
-  }
-  const characters = JSON.parse(body).characters;
-  for (let i = 0; i < characters.length; i++) {
-    request(characters[i], function (error, response, body) {
-      if (error) {
-        console.log(error);
-      }
-      console.log(JSON.parse(body).name);
-    });
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
   }
 });
